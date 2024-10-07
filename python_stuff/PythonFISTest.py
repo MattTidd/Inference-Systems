d43d40f99362afda7a3cbb54a6b6d6c396a69e58
MattTidd <mtidd2@unb.ca>
2024-10-07 13:22:19 -0300

    created test script for FIS

---
+++ python_stuff/PythonFISTest.py
@@ -0,0 +1,18 @@
+"""
+This program is for creating and spawning robots within a given
+task environment, such that tasks can be allocated to them using
+a fuzzy inference system.
+
+This is for testing the fuzzy inference system on fake, simulated 
+agents within a fictitious environment
+
+"""
+######################## Import Packages ########################
+
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import os
+from PythonFISImplementation import *
+
+#
