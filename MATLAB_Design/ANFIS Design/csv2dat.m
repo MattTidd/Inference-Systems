c3b09780f3ba923d3f7e25b7017544a7c92aa5ee
MattTidd <mtidd2@unb.ca>
2024-12-11 16:51:23 -0400

    started doing path stuff to access the CSVs

--- MATLAB_Design/ANFIS Design/csv2dat.m
+++ MATLAB_Design/ANFIS Design/csv2dat.m
@@ -13,21 +13,28 @@ clc ; clear ; close all ; format short;
 % Main:
 % *************************************************************************
 
+% get folder path:
+parentDirectory = fileparts(fileparts(cd))
+
+% tmr: get load the CSV data and turn into a .dat format
+
 % need to load the csv files and save them in a .dat format so that the
 % anfis app can load the data in:
 
-input_data = readmatrix('inputs.csv');
-output_data = readmatrix('outputs.csv');
+% data_folder_path = pwd
 
-data = [input_data output_data];
-
-writematrix(data, 'train_data.dat', 'Delimiter', ' ')
-writematrix(input_data, 'inputs.dat', 'Delimiter', ' ');
-writematrix(output_data, 'outputs.dat', 'Delimiter', ' ')
-
-% check that it worked:
-
-fid = fopen('inputs.dat');
-dat_data = fread(fid, '*char').';
-fclose(fid);
-disp(dat_data)
+% input_data = readmatrix('inputs.csv');
+% output_data = readmatrix('outputs.csv');
+% 
+% data = [input_data output_data];
+% 
+% writematrix(data, 'train_data.dat', 'Delimiter', ' ')
+% writematrix(input_data, 'inputs.dat', 'Delimiter', ' ');
+% writematrix(output_data, 'outputs.dat', 'Delimiter', ' ')
+% 
+% % check that it worked:
+% 
+% fid = fopen('inputs.dat');
+% dat_data = fread(fid, '*char').';
+% fclose(fid);
+% disp(dat_data)
